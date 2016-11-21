94ab8201c0224efc7452ad358da3201e8e3284a8
Alex Wong <alexwong024@gmail.com>
2016-11-21 11:23:52 -0500

    lobby scriptsg

---
+++ Assets/Utility/PanelController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelController : MonoBehaviour
+{
+  [SerializeField] GameObject battlePanel;
+  [SerializeField] GameObject gachaPanel;
+
+  public void DisplayBattle() {
+    EnablePanel(battlePanel, true);
+    EnablePanel(gachaPanel, false);
+  }
+
+  public void DisplayGacha() {
+    EnablePanel(gachaPanel, true);
+    EnablePanel(battlePanel, false);
+  }
+
+  public void DisplayLobby() {
+    EnablePanel(gachaPanel, false);
+    EnablePanel(battlePanel, false);
+  }
+
+  private void EnablePanel(GameObject panel, bool enable) {
+    panel.SetActive(enable);
+  }
+}
